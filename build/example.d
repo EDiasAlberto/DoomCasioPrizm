e291e02f27b7cf03e7e03ea40354dc3d938500d5
EDiasAlberto <retr0fade111@gmail.com>
2024-11-03 16:47:11 +0000

    initial push, with DOOM source code

---
+++ build/example.d
@@ -0,0 +1,6 @@
+example.o: \
+ H\:/PrizmSDK-win-0.6/PrizmSDK-win-0.6/projects/example/src/example.c \
+ H\:/PrizmSDK-win-0.6/PrizmSDK-win-0.6/include/fxcg/display.h \
+ H\:/PrizmSDK-win-0.6/PrizmSDK-win-0.6/include/fxcg/keyboard.h
+H\:/PrizmSDK-win-0.6/PrizmSDK-win-0.6/include/fxcg/display.h:
+H\:/PrizmSDK-win-0.6/PrizmSDK-win-0.6/include/fxcg/keyboard.h:
